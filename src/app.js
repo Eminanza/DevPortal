288c059b18a8950c0f33fca8da8f94deaa92df9f
Eminanza <michaeltihon@gmail.com>
2016-02-13 16:57:52 +0100

    Daily

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var NavBar = require('./navbar.js').NavBar;
 
 
 var App = React.createClass({
@@ -6,6 +7,7 @@ var App = React.createClass({
   render: function() {
     return (
         <div>
+          <NavBar />
         </div>
     );
   }
